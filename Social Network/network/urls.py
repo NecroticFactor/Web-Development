815a6b1ae705f404d20f3bf9171e28d6beba7769
Dr.A07 <157939154+NecroticFactor@users.noreply.github.com>
2025-01-08 00:49:15 +0530

    Update urls.py

--- Social Network/network/urls.py
+++ Social Network/network/urls.py
@@ -2,9 +2,14 @@ from django.urls import path
 
 from . import views
 
-urlpatterns = [
-    path("", views.index, name="index"),
+auth_patterns = [
     path("login", views.login_view, name="login"),
     path("logout", views.logout_view, name="logout"),
     path("register", views.register, name="register"),
 ]
+
+api_patterns = [
+    path("", views.index, name="index"),
+]
+
+urlpatterns = auth_patterns + api_patterns
