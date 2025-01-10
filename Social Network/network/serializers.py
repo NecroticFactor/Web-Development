bf2764383ea4d28b5e02ea212a22ac84b9c6fe96
Dr.A07 <157939154+NecroticFactor@users.noreply.github.com>
2025-01-10 18:51:59 +0530

    Update Serializers
    Made nested serializers for the hierarchy of the related models.

--- Social Network/network/serializers.py
+++ Social Network/network/serializers.py
@@ -18,7 +18,7 @@ class UserSerializer(serializers.ModelSerializer):
 
 
 class PostSerializer(serializers.ModelSerializer):
-    username = serializers.CharField(source="user.username", read_only=True)
+    user = UserSerializer(read_only=True)
     total_likes = serializers.IntegerField(read_only=True)
     total_comments = serializers.IntegerField(read_only=True)
 
@@ -26,7 +26,7 @@ class PostSerializer(serializers.ModelSerializer):
         model = Post
         fields = [
             "id",
-            "username",
+            "user",
             "title",
             "body",
             "total_likes",
@@ -36,33 +36,29 @@ class PostSerializer(serializers.ModelSerializer):
 
 
 class LikesSerializer(serializers.ModelSerializer):
-    username = serializers.CharField(source="user.username", read_only=True)
-    post = serializers.CharField(source="post.title", read_only=True)
-    post_id = serializers.CharField(source="post.id", read_only=True)
+    user = UserSerializer(read_only=True)
+    post = PostSerializer(read_only=True)
 
     class Meta:
         model = Likes
         fields = [
             "id",
-            "username",
+            "user",
             "post",
-            "post_id",
         ]
 
 
 class CommentsSerializer(serializers.ModelSerializer):
-    username = serializers.CharField(source="user.username", read_only=True)
-    post_title = serializers.CharField(source="post.title", read_only=True)
-    post_id = serializers.CharField(source="post.id", read_only=True)
+    user = UserSerializer(read_only=True)
+    post = PostSerializer(read_only=True)
     total_replies = serializers.IntegerField(read_only=True)
 
     class Meta:
         model = Comments
         fields = [
             "id",
-            "username",
-            "post_title",
-            "post_id",
+            "user",
+            "post",
             "comments",
             "total_replies",
             "created_at",
@@ -70,37 +66,29 @@ class CommentsSerializer(serializers.ModelSerializer):
 
 
 class RepliesSerializer(serializers.ModelSerializer):
-    username = serializers.CharField(source="user.username", read_only=True)
-    comment = serializers.CharField(source="comments.comments", read_only=True)
-    comment_id = serializers.CharField(source="comments.id", read_only=True)
+    user = UserSerializer(read_only=True)
+    comment = CommentsSerializer(read_only=True)
 
     class Meta:
         model = Replies
         fields = [
             "id",
-            "username",
+            "user",
             "comment",
-            "comment_id",
             "replies",
             "created_at",
         ]
 
 
 class FollowSerializer(serializers.ModelSerializer):
-    followed_username = serializers.CharField(
-        source="followed.username", read_only=True
-    )
-    follower_username = serializers.CharField(
-        source="follower.username", read_only=True
-    )
+    follower = UserSerializer(read_only=True)
+    followed = UserSerializer(read_only=True)
 
     class Meta:
         model = Follow
         fields = [
             "id",
-            "follower_username",
             "follower",
-            "followed_username",
             "followed",
             "status",
             "created_at",
@@ -120,19 +108,18 @@ class FollowUpdateSerializer(serializers.Serializer):
 
 
 class BlockSerializer(serializers.ModelSerializer):
-    blocked_username = serializers.CharField(source="blocked.username", read_only=True)
-    blocker_username = serializers.CharField(source="blocker.username", read_only=True)
+    blocker = UserSerializer(read_only=True)
+    blocked = UserSerializer(read_only=True)
     blocked_id = serializers.IntegerField(write_only=True)
 
     class Meta:
         model = Blocked
         fields = [
-            "blocked_username",
+            "blocker",
+            "blocked",
             "blocked_id",
-            "blocker_username",
-            "blocker_id",
             "created_at",
         ]
         read_only = [
-            "blocker_id",
+            "blocker",
         ]
